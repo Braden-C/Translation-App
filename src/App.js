447b385207fb002d04a57a7567ac092cf8a34f99
Braden C <bradencchapman@yahoo.com>
2022-03-03 13:28:01 -0700

    basic styling no new lang

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Dropdown from './components/Dropdown'
 import Translate from './components/Translate'
 
+
 const App = () => {
 
     return (
--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Dropdown from "./Dropdown";
 import Convert from "./Convert";
+import './App.css'
 
 const options = [
     {
@@ -24,11 +25,11 @@ const Translate = () => {
     const [text, setText] = useState('');
 
     return (
-        <div>
+        <div className="main">
             <div className="ui form">
                 <div className="field">
                     <label>Enter Text</label>
-                    <input value={text} onChange={(e) => setText(e.target.value)} />
+                    <input classname="text input" value={text} onChange={(e) => setText(e.target.value)} />
                 </div>
             </div>
             <Dropdown
@@ -37,7 +38,7 @@ const Translate = () => {
                 selected={language}
                 onSelectedChange={setLanguage} />
             <hr />
-            <h3 className="ui header">Output</h3>
+            <h3 className="ui header label">Translation:</h3>
             <Convert text={text} language={language} />
 
         </div>
